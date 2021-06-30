4c734d572b5663f0eba82160f00a6ad9a1d16f3f
Rabz92 <rabbiailyas92@gmail.com>
2021-06-30 13:24:09 +0200

    added new url

--- src/server.js
+++ src/server.js
@@ -1,45 +1,57 @@
-import express from "express"
-import listEndpoints from "express-list-endpoints"
-import cors from 'cors'
-import { join } from "path"
-import productsRouter from './services/products/index.js'
-import { catchAllErrors, badRequestMiddleware, notFoundMiddleWare } from "./errorMiddlewares.js"
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import { join } from "path";
+import productsRouter from "./services/products/index.js";
+import {
+  catchAllErrors,
+  badRequestMiddleware,
+  notFoundMiddleWare,
+} from "./errorMiddlewares.js";
 /* import { getCurrentFolderPath } from "./lib/fs-tools.js" */
 
-const PORT = process.env.PORT || 3005
-const server = express()
+const PORT = process.env.PORT || 3005;
+const server = express();
 /* const publicFolderPath = join(getCurrentFolderPath(import.meta.url), "../public")
  */
-const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_PROD_URL, process.env.VERCEL_URL]
+const whitelist = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_PROD_URL,
+  process.env.VERCEL_URL,
+  process.env.VERCEL_URL_SECOND,
+];
 console.log(process.env.FRONTEND_URL);
 /* ************MIDDLEWARES***************** */
 
 /* server.use(express.static(publicFolderPath)) */
-server.use(express.json())
-server.use(cors({
+server.use(express.json());
+server.use(
+  cors({
     origin: (origin, callback) => {
-        if(!origin || whitelist.indexOf(origin) !== -1){
-            callback(null, true)
-        }
-        else{
-            callback(new Error("not allowed by cors"))
-        }
-    }
-}))
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("not allowed by cors"));
+      }
+    },
+  })
+);
 
 /* ************ENDPOINTS******************* */
 
-server.use("/products", productsRouter)
+server.use("/products", productsRouter);
 
 /* ***********ERROR MIDDLEWARES************ */
 
-server.use(notFoundMiddleWare)
-server.use(badRequestMiddleware)
-server.use(catchAllErrors)
+server.use(notFoundMiddleWare);
+server.use(badRequestMiddleware);
+server.use(catchAllErrors);
 
 console.table(listEndpoints(server));
-server.listen(PORT,()=>{
-    console.log("🧡 server is running on port: " + PORT);
-})
+server.listen(PORT, () => {
+  console.log("🧡 server is running on port: " + PORT);
+});
 
-server.on('error', (error)=> console.log('💔 Server is not running due to 🛠', error))
+server.on("error", (error) =>
+  console.log("💔 Server is not running due to 🛠", error)
+);
